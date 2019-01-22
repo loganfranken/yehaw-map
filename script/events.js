b6ed440f62369ece8391fff1d1ee8c9c4c454151
Logan Franken <loganfranken@gmail.com>
2019-01-21 23:28:03 -0800

    Fixed a few locations

--- script/events.js
+++ script/events.js
@@ -5,136 +5,136 @@
     SeattlePublicLibrary: {
       title: 'Seattle Public Library',
       coordinates: {
-        lat: 47.6066004,
-        lng: -122.3351724
+        lat: 47.606682,
+        lng: -122.332657
       }
     },
 
     AlmaMaterTacoma: {
       title: 'Alma Mater Tacoma',
       coordinates: {
-        lat: 47.2499213,
-        lng: -122.4449067
+        lat: 47.249960,
+        lng: -122.442700
       }
     },
 
     SeattleCenter: {
       title: 'Seattle Center',
       coordinates: {
-        lat: 47.6207455,
-        lng: -122.352622
+        lat: 47.621937,
+        lng: -122.351746
       }
     },
 
     FeastArtsCenter: {
       title: 'Feast Arts Center',
       coordinates: {
-        lat: 47.2512975,
-        lng: -122.4584846
+        lat: 47.251320,
+        lng: -122.456190
       }
     },
 
     VermillionGallery: {
       title: 'Vermillion Gallery',
       coordinates: {
-        lat: 47.6143814,
-        lng: -122.3201103
+        lat: 47.614430,
+        lng: -122.317750
       }
     },
 
     KingStreetStation: {
       title: 'King Street Station',
       coordinates: {
-        lat: 47.5983171,
-        lng: -122.3320368
+        lat: 47.598419,
+        lng: -122.330093
       }
     },
 
     OlympicSculpturePark: {
       title: 'Olympic Sculpture Park',
       coordinates: {
-        lat: 47.6165958,
-        lng: -122.3553101
+        lat: 47.616610,
+        lng: -122.354930
       }
     },
 
     SeattleArtMuseum: {
       title: 'Seattle Art Museum',
       coordinates: {
-        lat: 47.607309,
-        lng: -122.3403218
+        lat: 47.607288,
+        lng: -122.337860
       }
     },
 
     HedreenGallery: {
       title: 'Hedreen Gallery',
       coordinates: {
-        lat: 47.5959878,
-        lng: -122.3194932
+        lat: 47.610100,
+        lng: -122.316360
       }
     },
 
     SouthPugetSoundCommunityCollegeGallery: {
       title: 'South Puget Sound Community College Gallery',
       coordinates: {
-        lat: 47.0237083,
-        lng: -122.9317756
+        lat: 47.022610,
+        lng: -122.931107
       }
     },
 
     WashingtonStateHistoryMuseum: {
       title: 'Washington State History Museum',
       coordinates: {
-        lat: 47.2447903,
-        lng: -122.4381433
+        lat: 47.245209,
+        lng: -122.436340
       }
     },
 
     AmazonVanVorstPlaza: {
       title: 'Amazon Van Vorst Plaza',
       coordinates: {
-        lat: 47.622634,
-        lng: -122.3383986
+        lat: 47.623210,
+        lng: -122.337160
       }
     },
 
     UniversityOfPugetSound: {
       title: 'University of Puget Sound',
       coordinates: {
-        lat: 47.2639174,
-        lng: -122.4833651
+        lat: 47.263880,
+        lng: -122.479050
       }
     },
 
     UnionClub: {
       title: 'Union Club',
       coordinates: {
-        lat: 47.2586859,
-        lng: -122.4435621
+        lat: 47.258760,
+        lng: -122.441350
       }
     },
 
     ChiefSeattleClub: {
       title: 'Chief Seattle Club',
       coordinates: {
-        lat: 47.6012206,
-        lng: -122.3332812
+        lat: 47.601210,
+        lng: -122.331140
       }
     },
 
     DaybreakStarCulturalCenter: {
       title: 'Daybreak Star Cultural Center',
       coordinates: {
-        lat: 47.6679574,
-        lng: -122.4201926
+        lat: 47.667660,
+        lng: -122.413030
       }
     },
 
     SuquamishMuseum: {
       title: 'Suquamish Museum',
       coordinates: {
-        lat: 47.7293019,
-        lng: -122.5576171
+        lat: 47.729270,
+        lng: -122.557170
       }
     }
 
